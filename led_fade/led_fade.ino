967792fb43d9a11c8042154e9f5a37aae8273e5f
ARPIT KUMAR <arpitk2001@gmail.com>
2021-12-17 19:33:26 +0530

    initial commit

---
+++ led_fade/led_fade.ino
@@ -0,0 +1,26 @@
+//int m = 1;
+void setup() 
+{
+  pinMode(10, OUTPUT);
+  //Serial.begin(9600);
+}
+
+void loop() 
+{
+  for(int i = 0; i<=255; i++)
+  {
+    //Serial.println(m);
+    delay(10);
+    analogWrite(10, i);
+  }
+
+  for(int i = 255; i>=0; i--)
+  {
+    //Serial.println(m);
+    delay(10);
+    analogWrite(10, i);
+  }
+
+}
+
+  
